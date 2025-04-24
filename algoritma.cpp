83bf76859632e37cdbd4cee99f5e9cb9dde4d01d
RizkyPalaguna <rizky23458@gmail.com>
2025-04-24 15:47:15 +0700

    Add files via upload

---
+++ algoritma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	string kata;
+	cout<<"Masukan kata = HALO"<<endl;
+	cin>>kata;
+	
+	if (kata=="HALO"){
+		cout <<"kata yang dimasukan sesuai"<<endl;
+	}else{
+		cout<<"kata yang dimasukan tidak sesuai"<<endl;
+	}
+}
